69b87bc6aa2b93b6fa1e8721bdeb3e63010f385c
basher113 <147272612+Basher113@users.noreply.github.com>
2025-05-19 16:19:28 +0800

    Create playRound function that checks the result
    - Increment score of the winner of the round
    - Display the result of the round in the console

--- script.js
+++ script.js
@@ -17,3 +17,46 @@ let humanScore = 0;
 let computerScore = 0;
 const computerSelection = getComputerChoice();
 const humanSelection = getHumanChoice();
+
+const playRound = (humanChoice, computerChoice) => {
+    const winMessage = `You won! ${humanChoice} beats ${computerChoice}` 
+    const loseMessage = `You lose! ${computerChoice} beats ${humanChoice}`
+
+    if (humanChoice === computerChoice) {
+        console.log(`Tie! You both chose ${humanChoice}`)
+        return;
+    } 
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            console.log(winMessage);
+            humanScore++;
+        } else if (computerChoice === "paper") {
+            console.log(loseMessage);
+            computerScore++;
+        }
+
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock" ) {
+            console.log(winMessage);
+            humanScore++;
+        } else if (computerChoice === "scissors") {
+            console.log(loseMessage);
+            computerScore++;
+        }
+
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            console.log(winMessage);
+            humanScore++;
+        }
+        else if (computerChoice === "rock" ) {
+            console.log(loseMessage);
+            computerScore++;
+        }
+    } else {
+        console.log("Invalid Input.");
+    }
+}
+
+playRound(humanSelection, computerSelection)
